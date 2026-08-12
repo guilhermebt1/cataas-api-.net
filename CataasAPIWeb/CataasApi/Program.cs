1831d7dfb8687ff0ad85169419efcfaa126970fd
Guilherme <185707585+guilhermebt1@users.noreply.github.com>
2026-08-12 12:16:03 -0300

    program.cs migrations

--- CataasAPIWeb/CataasApi/Program.cs
+++ CataasAPIWeb/CataasApi/Program.cs
@@ -39,6 +39,12 @@ namespace CataasApi
             
 
             var app = builder.Build();
+			
+			using (var scope = app.Services.CreateScope())
+			{
+				var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+				dbContext.Database.Migrate();
+			}
             app.UseExceptionHandler(errorApp =>
             {
                 errorApp.Run(async context =>
